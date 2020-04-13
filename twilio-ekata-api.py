f5c10d27cb226ec83549575d114446fa04969c22
Jerome Dixon <jerome.dixon90@gmail.com>
2020-04-12 23:31:36 -0400

    Update twilio-ekata-api.py

--- twilio-ekata-api.py
+++ twilio-ekata-api.py
@@ -6,7 +6,7 @@ auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
 
 phone_number = client.lookups \
-    .phone_numbers('+18048733575') \
+    .phone_numbers('+1804xxxxxxx') \
     .fetch(add_ons=['ekata_reverse_phone'], type=['carrier'])
 
 print(phone_number.add_ons)

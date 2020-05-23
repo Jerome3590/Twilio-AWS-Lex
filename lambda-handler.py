5669e832857006f0e487f13b1133a6ec90c8e6bf
Jerome Dixon <jerome.dixon90@gmail.com>
2020-05-23 01:17:15 -0400

    Update lambda-handler.py

--- lambda-handler.py
+++ lambda-handler.py
@@ -4,6 +4,7 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+
 # ---Main Handler---
 def lambda_handler(event, context):
     logger.debug(event)
@@ -29,104 +30,92 @@ def lambda_handler(event, context):
         logger.debug(event)
 
 
-def input_response(slots_lex):
+# Helper functions
+def listToDict(lst): 
+    dict_res = dict.fromkeys(lst,)
+    return dict_res 
+
+
+def input_response(slots_updated):
     response = {
         'dialogAction': {
             'type': "Delegate",
-            'slots': slots_lex
+            'slots': slots_updated
         }
     }
     return response
 
 
-def fulfillment_response(session_attributes, fullfillment_state, message):
-    filled_response = {
-        'sessionAttributes': session_attributes,
+def fulfillment_response():
+    fullfill_response = {
         'dialogAction': {
             'type': "Close",
-            'fulfillmentState': fullfillment_state,
-            'message': message + '. Survey is now complete. Clinician will review responses and send gift card to '
-                                 'number on file'
+            'fulfillmentState': "Fulfilled",
+            'message': "Survey is now complete. Clinician will review responses and send gift card to number on file"
         }
     }
-    return filled_response
-
-
-def merge_slots(dict1, dict2):
-    d1 = dict1['Slots']
-    d2 = dict2['Slots']
-    d1.update(d2)
-    res = d1
-    return res
+    return fulfill_response
 
 
 def validate_input(event):
-    try:
-        slots_current_all = event['currentIntent']['slots']
-        logger.debug(slots_current_all)
-        slots_current_no_nulls = list({ele for ele in slots_current_all if slots_current_all[ele]})
-        logger.debug(slots_current_no_nulls)
-        slots = {'Slots': slots_current_no_nulls}
-        logger.debug(slots)
+    slots_dict = event['currentIntent']['slots']
+    logger.debug(slots_dict)
+    
 
-        if slots.get('raceOne') != '4':
-            slots_1 = {
-                'Slots': {
-                    'raceTwo': '8',
-                    'raceThree': '12'
-                }
+    if slots_dict.get('raceOne') == '1' or slots_dict.get('raceOne') == '2' or slots_dict.get('raceOne') == '3':
+        slots_1 = {
+            'Slots': {
+                'raceTwo': '8',
+                'raceThree': '12'
             }
-            slots_lex = merge_slots(slots_1, slots)
-            return input_response(slots_lex)
+        }
+        s1 = slots_1["Slots"]
+        slots_dict.update(s1)
 
-        elif slots.get('milStatus') == '1':
-            slots_2 = {
-                'Slots': {
-                    'milStatusTwo': '1',
-                    'milService': '5',
-                    'milDeployment': '1'
-                }
+    if slots_dict.get('milStatus') == '1':
+        slots_2 = {
+            'Slots': {
+                'milStatusTwo': '1',
+                'milService': '5',
+                'milDeployment': '1'
             }
-            slots_lex = merge_slots(slots_2, slots)
-            return input_response(slots_lex)
+        }
+        s2 = slots_2["Slots"]
+        slots_dict.update(s2)
 
-        elif slots.get('activitySexOne') == '2':
-            slots_3 = {
-                'Slots': {
-                    'activitySexTwo': '1',
-                    'activitySexThree': '0',
-                    'activitySexFour': '0',
-                    'activitySexFive': '0',
-                    'activitySexSix': '0'
-                }
+    if slots_dict.get('activitySexOne') == '2':
+        slots_3 = {
+            'Slots': {
+                'activitySexTwo': '1',
+                'activitySexThree': '0',
+                'activitySexFour': '0',
+                'activitySexFive': '0',
+                'activitySexSix': '0'
             }
-            slots_lex = merge_slots(slots_3, slots)
-            return input_response(slots_lex)
-
-        else:
-            raise ValueError('No Data Validation Conditions Met')
-    except ValueError:
-        return input_response(slots_current_all)
+        }
+        s3 = slots_3["Slots"]
+        slots_dict.update(s3)
+    
+    return input_response(slots_dict)
 
 
 def complete_survey(event):
     logger.debug(event)
     userID = event.get('userId')
+    logger.debug(userID)
     intent = event['currentIntent']['name']
-    sessionID = event.get('sessionId')
     slots = event['currentIntent']['slots']
-    
+
     # DynamoDB client for posting final survey response
     client = boto3.resource("dynamodb")
-    table = client.Table("DBHDS_YSAT")
+    table = client.Table("YSAT")
 
     table.put_item(
         Item={
             'IntentName': intent,
-            'SessionID': sessionID,
             'UserID': userID,
             'Slots': slots
         }
     )
 
-    return fulfillment_response(message)
+    return fulfillment_response
